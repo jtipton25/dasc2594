41555c1d8b0cf4a4425bfe71fc8d366f56f79f8d
jtipton25 <jtipton25@users.noreply.github.com>
2021-03-10 16:38:48 -0600

    added elementary matrix function

--- R/array_to_latex.R
+++ R/array_to_latex.R
@@ -1,20 +1,29 @@
 #' Convert an R array into \LaTeX for easy writing in Rmarkdown
 #'
 #' @param A An array to convert to Latex format
+#' @param fraction An option of whether to print the result as a decimal or fracion
 #'
 #' @return A string that can be put into an Rmarkdown document by using `cat()` to direct the output
 #' @export
+#' @importFrom MASS fractions
 #'
 #' @examples
 #' latex_string <- array_to_latex(matrix(1:6, 3, 2))
 #'
 #' # within Rmarkdown chunk
 #' # cat(latex_string)
-array_to_latex <- function(A){
+array_to_latex <- function(A, fraction = FALSE){
     # From https://data-and-the-world.onrender.com/posts/matrix-to-latex/
     if (!is.matrix(A))
         stop("Input parameter 'A' must be a matrix.")
-    rows <- apply(A, MARGIN = 1, paste, collapse = " & ")
+    if (!is.logical(fraction))
+        stop("fraciton must be a logical input.")
+    rows <- NULL
+    if (fraction) {
+        rows <- apply(as.character(fractions(A)), MARGIN = 1, paste, collapse = " & ")
+    } else {
+        rows <- apply(A, MARGIN = 1, paste, collapse = " & ")
+    }
     matrix_string <- paste(rows, collapse = " \\\\ ")
     return(paste("\\begin{pmatrix}", matrix_string, "\\end{pmatrix}"))
 }
---
+++ R/elementary_matrix.R
@@ -0,0 +1,70 @@
+#' Calculate a random elementary matrix
+#'
+#' @param n The size of the elementary matrix (an n times n matrix)
+#' @param verbose A logical value that determines whether to print the output
+#' @param operation If specifiec, operation is a character string specifying what kind of elementary row operation to perform. The options are "row swap", "scalar multiplication", and "row addition".
+#' @return An elementary matrix
+#' @export
+#'
+elementary_matrix <- function(n, operation = NULL, verbose = FALSE) {
+    # add in option to get a specific row operation
+
+    # check if a valid operation
+    if (is.null(operation)) {
+        operation <- sample(c("row swap", "scalar multiplication", "row addition"), 1)
+    }
+    if (!(operation %in% c("row swap", "scalar multiplication", "row addition")))
+        stop('operation must be either "row swap", "scalar multiplication", or "row addition"')
+    # initialize the elementary matrix
+    E <- diag(n)
+
+    if (operation == "scalar multiplication"){
+        if (verbose)
+            message("scalar multiplication")
+        #scalar row mult
+        row_idx <- sample(1:n, 1, replace = FALSE)
+        E[row_idx, ] <- sample((-9:9)[-10], 1) * E[row_idx, ]
+    } else if (operation == "row swap") {
+        if (verbose)
+            message("row swap")
+        #row swaps
+        #choose two rows, getting two distinct rows
+        row_index <- sample(1:n, 2, replace = FALSE)
+        E[row_index, ] <- E[rev(row_index), ]
+    } else {
+        if (verbose)
+            message("row multiplication and addition")
+        # row mult and addition, mult one row by something, mult by other row, and add to other
+        # sample(-9:9, 1)
+        # row_index[1]
+        # row_index[2]
+        row_index <- sample(1:n, 2, replace = FALSE)
+        E[row_index[1],] <- E[row_index[1],] + sample((-9:9)[-10], 1) * E[row_index[2],]
+    }
+
+    return(E)
+
+}
+
+#' Calculate a random elementary matrix
+#'
+#' @param n The size of the elementary matrix (an n times n matrix)
+#' @param k The number of elemenatry row operations to perform
+#' @param verbose A logical value that determines whether to print the output
+#' @return An product of elementary matrices, the inverse of the produce of elementary matrices, and the full sequence of elementary matrices
+#' @export
+#'
+elementary_matrix_sequence <- function(n, k, verbose = FALSE) {
+    # n is the size of the matrix, k is the number of operations to perform
+    E <- diag(n)
+    E_inv <- diag(n)
+    matrix_sequence <- vector(mode = "list", length = k)
+    for (j in 1:k) {
+        matrix_sequence[[j]] <- elementary_matrix(n, verbose = verbose)
+        E <- E %*% matrix_sequence[[j]]
+        E_inv <- solve(matrix_sequence[[j]]) %*% E_inv
+    }
+    return(list(E = E, E_inv = E_inv, matrix_sequence = matrix_sequence))
+}
+
+
